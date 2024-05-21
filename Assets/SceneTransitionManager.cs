beffc44ac9c232d76583d760355bb27a2b0925d4
Stephan Pölzl <p.stephan@gmx.at>
2024-05-21 15:03:00 +0200

    Added a transition manager and a tutorial scene

--- Assets/SceneTransitionManager.cs
+++ Assets/SceneTransitionManager.cs
@@ -1,12 +1,41 @@
 using System.Collections;
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransitionManager : MonoBehaviour
 {
-    public void GoToSceneAsync(int sceneIndex)
+    [SerializeField] private int sceneToLoadIndex;
+    [SerializeField] private Button transitionButton;
+
+    private void Start()
     {
-        SceneManager.LoadSceneAsync(sceneIndex);
+        if (transitionButton != null)
+        {
+            transitionButton.onClick.AddListener(OnTransitionButtonClicked);
+        }
+        else
+        {
+            Debug.LogError("Transition Button is not assigned in the Inspector");
+        }
+    }
+
+    private void OnTransitionButtonClicked()
+    {
+        Debug.Log("Transition button clicked");
+        StartCoroutine(TransitionToScene());
+    }
+
+    private IEnumerator TransitionToScene()
+    {
+        // Load the scene asynchronously by build index
+        Debug.Log("Loading scene index: " + sceneToLoadIndex);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoadIndex);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
